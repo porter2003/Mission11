bf77c341721e1c48b4d2e983aedfacb63f6d00dc
porter2003 <porteralyman@gmail.com>
2025-03-19 20:20:32 -0600

    Initial commit

--- backend/Mission11.API/Controllers/BookController.cs
+++ backend/Mission11.API/Controllers/BookController.cs
@@ -16,6 +16,39 @@ namespace Mission11.API.Controllers
 
 
         [HttpGet("GetBooks")]
-        public IEnumerable<Book> GetBooks() => _bookContext.Books.ToList();
+        public IActionResult GetBooks(int pageSize, int pageNum = 1, string sortOrder = "asc")
+        {
+            //var something = _bookContext.Books
+            //    .Skip((pageNum-1)*pageSize)
+            //    .Take(pageSize)
+            //    .ToList();
+            var booksQuery = _bookContext.Books.AsQueryable();
+
+            // Apply sorting
+            if (sortOrder == "asc")
+            {
+                booksQuery = booksQuery.OrderBy(b => b.Title);
+            }
+            else
+            {
+                booksQuery = booksQuery.OrderByDescending(b => b.Title);
+            }
+
+            var something = booksQuery
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var totalNumBooks = _bookContext.Books.Count();
+
+            var someObject = new
+            {
+                Books = something,
+                TotalNumBooks = totalNumBooks
+            };
+
+            return Ok(someObject);
+
+        } 
     }
 }
--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d83347b7bb9106cc08f165720bf28e9634d7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
